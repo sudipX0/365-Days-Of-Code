acec97eae1350cc2e0fa9b5dc7e33a9d7c64614d
sudipX0 <sudippoudel@sd.taylors.edu.my>
2025-05-01 17:39:16 +0545

    Colab Repo Added

--- Algorithms/Sorting/BubbleSort.py
+++ Algorithms/Sorting/BubbleSort.py
@@ -1,10 +1,14 @@
 my_arr = [5, 3, 8, 4, 2]
-
+swap = True
 for i in range(1, len(my_arr)):
     pointer = 0
     print(f"Pass: {i}")
     for j in range(1, len(my_arr)):
         if my_arr[pointer] > my_arr[pointer + 1]:
-            my_arr[pointer], my_arr[pointer + 1] = my_arr[pointer + 1], my_arr[pointer]
+            my_arr[pointer], my_arr[pointer + 1] = (
+                my_arr[pointer + 1],
+                my_arr[pointer],
+            )
+
         print(my_arr)
         pointer += 1
